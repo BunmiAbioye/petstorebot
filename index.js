1e39b76ffcf53c81a6d7e5cc822909c92f588096
BunmiAbioye <bunmioluwa@gmail.com>
2021-04-30 23:44:27 -0400

    editted

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/payment", upload.none(), (req, res) => {
   console.log("FomData" + req.form);
   let sFrom = req.body.phone;
   console.log("Form body " + JSON.stringify(sFrom));
-  let sUrl = "https://popup-restaurant.herokuapp.com/payment"
+  let sUrl = "http://localhost:3002/payment"
   console.log("URL - " + sUrl)
   oOrders[sFrom] = new RestaurantOrder(sFrom, sUrl);
   oOrders[sFrom].totalprice = 10;
